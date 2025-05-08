e70dfd15b935f4ef268a35505576a2e0a976c54c
Daniel <sotodaniel531@gmail.com>
2025-05-07 23:45:15 -0700

    Set up simple streamlit app that parses the csv file (wells fargo)

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,24 @@ import os
 
 st.set_page_config(page_title="Personal Expense Dashboard", page_icon="./black-logo.png")
 
+def parse_csv(file) -> any:
+    try:
+        df = pd.read_csv(file, header=None, names=["Date", "Amount", "Status", "Check_Number", "Description"], dtype=str)
+        df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%Y", errors="coerce").dt.date
+        df["Amount"] = (
+            df["Amount"]
+            .str.replace(r"[\$,]", "", regex=True)
+            .astype(float)
+        )
+        
+        # 4. Label each transaction as 'expense' or 'surplus'
+        df["Type"] = df["Amount"].apply(lambda x: "expense" if x < 0 else "surplus")
+        
+        return df
+    except Exception as e:
+        print(e)
+        return None
+
 def start_dashboard():
     st.title("Personal Finance Dashboard")
 
@@ -14,6 +32,7 @@ def start_dashboard():
 
 
     if statement is not None:
-        print("Statement")
+        df = parse_csv(statement)
+        df 
 if __name__ == "__main__":
     start_dashboard()
